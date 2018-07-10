700c23a24ce83719a62c95389e655c5e615ad330
Admin <admin@Ebolaa.local>
2018-07-10 08:00:19 +0700

    add relationship

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,22 @@
 class RelationshipsController < ApplicationController
+	def create
+		followed = check_followed params[:follow][:monitored_id]
+		if followed == nil
+			relationship = current_user.relationships.new relationship_params
+			if relationship.save
+			end
+		end
+	end
+
+	def destroy
+		followed = Relationship.find_by id: params[:followed][:id]
+		byebug
+		if followed != nil
+			followed.destroy
+		end
+	end
+
+	def relationship_params
+		params.require(:follow).permit Relationship::ATTRIBUTES_PARAMS
+	end
 end
